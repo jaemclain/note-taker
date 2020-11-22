265ab3331a3a7bb0db0a9eecaaa97d54a36f661d
Jae McLain <jae.mclain@gmail.com>
2020-11-21 21:01:08 -0800

    changed location of routes to server.js

--- server.js
+++ server.js
@@ -1,25 +1,93 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { response } = require("express");
 
+
+// Express Server Setup
 const app = express();
 
-// Set PORT to 8080
-var PORT = process.env.PORT || 8080;
+// Port Setup
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
-require("./routes/apiRoutes")(app);
-app.use("/api", apiRoutes);
+// Routes API & HTML
+// require("./Develop/routes/apiRoutes")(app);
+// require("./Develop/routes/htmlRoutes")(app)
 
-require("./routes/htmlRoutes")(app);
-app.use(htmlRoutes)
+var notesArr = [];
+
+// Route Index
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Returns notes.html
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
 
-// The below code effectively "starts" our server
-app.listen(PORT, function() {
+// GET ROUTE - ALL notes
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function (err, data) {
+    if (err) {
+      throw err
+    }
+  })
+  return response.json(JSON.parse(data));
+});
+
+
+// GET Request - ID
+app.get("/api/notes/:id", function (req, res) {
+  res.json(notes[req.params.id]);
+});
+
+
+
+// POST ROUTE - Create New Note
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+  newNote.id = noteId;
+  notes.push(newNote);
+  fs.writeFile("./db/db.json", `[${notesArr}]`, 'utf-8', function (err) {
+    if (err) {
+      throw err
+    } return response.json(request.body);
+  })
+});
+
+
+// Delete Note
+app.delete("/api/notes/:id", function (req, res) {
+  var sorting = req.params.id;
+
+  fs.readFile("./db/db.json", function(err,data){
+    if(err){
+      throw err
+    }
+    var notesHistory = JSON.parse(data);
+    for(let i=0; i < notesArr.length; i++) {
+      if(sorting == notesHistory[i].id){
+        notesArr.splice(i,1)
+        fs.writeFile("./db/db.json",`[${notesArr}]`, 'utf-8', function(err){
+          if(err){
+            throw err
+          }return
+        })
+      }
+    }
+    })
+    response.end()
+  });
+
+
+  // Starts the server
+  app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
   });
-  
