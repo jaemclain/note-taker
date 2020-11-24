bf9fe37e5bc7b4af86d28511229f321874315821
Jae McLain <jae.mclain@gmail.com>
2020-11-23 20:26:01 -0800

    fixed issues

--- server.js
+++ server.js
@@ -10,84 +10,81 @@ const app = express();
 // Port Setup
 var PORT = process.env.PORT || 3000;
 
+var notesArr = [];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-// Routes API & HTML
-// require("./Develop/routes/apiRoutes")(app);
-// require("./Develop/routes/htmlRoutes")(app)
-
-var notesArr = [];
 
 // Route Index
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
 // Returns notes.html
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+app.get("/notes", function (request, response) {
+  response.sendFile(path.join(__dirname, "./public/notes.html"));
+})
 
 
 // GET ROUTE - ALL notes
-app.get("/api/notes", function (req, res) {
-  fs.readFile("./db/db.json", function (err, data) {
-    if (err) {
+app.get("/api/notes", function (request, response) {
+  fs.readFile("./db/db.json", function (err, data){
+    if(err){
       throw err
     }
+    return response.json(JSON.parse(data));
   })
-  return response.json(JSON.parse(data));
-});
-
-
-// GET Request - ID
-app.get("/api/notes/:id", function (req, res) {
-  res.json(notes[req.params.id]);
-});
-
+})
 
 
 // POST ROUTE - Create New Note
-app.post("/api/notes", function (req, res) {
-  var newNote = req.body;
-  newNote.id = noteId;
-  notes.push(newNote);
+app.post("/api/notes", function (request, response) {
+  request.body["id"] = notesArr.length + 1;
+  var newNote = JSON.stringify(request.body);
+  // newNote.id = noteId;
+  notesArr.push(newNote);
+
   fs.writeFile("./db/db.json", `[${notesArr}]`, 'utf-8', function (err) {
     if (err) {
       throw err
-    } return response.json(request.body);
+    } 
+    return response.json(request.body);
   })
 });
 
 
 // Delete Note
-app.delete("/api/notes/:id", function (req, res) {
+app.delete("/api/notes/:id", function(request, response){
   var sorting = req.params.id;
 
-  fs.readFile("./db/db.json", function(err,data){
-    if(err){
+  fs.readFile("./db/db.json", function(err, data){
+    if (err){
       throw err
     }
+
     var notesHistory = JSON.parse(data);
-    for(let i=0; i < notesArr.length; i++) {
+    
+    for(let i = 0; i < notesArr.length; i++){
       if(sorting == notesHistory[i].id){
-        notesArr.splice(i,1)
-        fs.writeFile("./db/db.json",`[${notesArr}]`, 'utf-8', function(err){
-          if(err){
+        notesArr.splice(i, 1)
+
+        fs.writeFile("./db/db.json", `[${notesArr}]`, 'utf-8', function(err){
+          if (err) {
             throw err
-          }return
+          }
+          return
         })
       }
     }
-    })
-    response.end()
-  });
+  })
+  response.end()
+});
 
 
-  // Starts the server
-  app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-  });
+// Starts the server
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
